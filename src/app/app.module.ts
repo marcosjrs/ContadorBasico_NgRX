2cf02d127ca21d5da428a7c670a6ec7bd488539d
marcosjrs <marcosjrs80@gmail.com>
2019-01-19 12:11:15 +0100

    Creación del Reducer e integración en la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
+import { contadorReducer } from './contador/contador.reducer';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({count:contadorReducer})// {count:contadorReducer} es el State Principal de toda la aplicación (es toda la estructura de datos)
   ],
   providers: [],
   bootstrap: [AppComponent]
