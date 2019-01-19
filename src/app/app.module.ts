6debe2de6203396f63bc8aa563c168960aa071e5
marcosjrs <marcosjrs80@gmail.com>
2019-01-19 18:02:41 +0100

    Añadir el StoreDevtoolsModule para poder utilizar el plugin de Chrome..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
 import { contadorReducer } from './contador/contador.reducer';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,11 @@ import { contadorReducer } from './contador/contador.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({contador:contadorReducer})// {contador:contadorReducer} es el State Principal de toda la aplicación (es toda la estructura de datos)
+    StoreModule.forRoot({contador:contadorReducer}),// {contador:contadorReducer} es el State Principal de toda la aplicación (es toda la estructura de datos)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, //Retiene 25 estados
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
