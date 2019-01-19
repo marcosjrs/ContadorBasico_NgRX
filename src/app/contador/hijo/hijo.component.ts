78d78eee0032204e7567940e1d05ea38d5080ce7
marcosjrs <marcosjrs80@gmail.com>
2019-01-19 11:11:36 +0100

    Componente principal  que incrementa y decrementa

---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styles: []
+})
+export class HijoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
